a45115c313ad488831a61a6426c7b0eee4a79621
mfolwh <mfolwh@MacBook-Air-mfolwh.local>
2026-07-31 17:10:03 +0300

    fix: admin panel, edit workout blank screen, cloud load partial permissions

--- scratch/check_easter_egg.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('/Users/mfolwh/Desktop/TRA/app.js', 'r') as f:
-    js = f.read()
-
-matches = re.finditer(r'.{0,50}EasterEgg.{0,50}', js, re.IGNORECASE)
-for m in matches:
-    print(m.group(0))
-
